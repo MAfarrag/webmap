a969cd12f1259a5518e5792cc81ede0c57cd0193
Mostafa Farrag <moah.farag@gmail.com>
2021-09-12 14:36:08 +0200

    - change location of the server file

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os, sys
 # os.chdir("F:/01Algorithms/webmap")
 import http.server
 import socketserver
+
 rootpath = os.path.abspath(os.getcwd())
 sys.path.append(rootpath + "/src")
 datapath = os.path.join(rootpath, "data")
@@ -16,7 +17,7 @@ class RequestsHandler(http.server.SimpleHTTPRequestHandler):
   """
   def do_GET(self):
     if self.path == '/':
-      self.path = 'RCS.html'
+      self.path = 'src/RCS.html'
     return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 handler_object = RequestsHandler
